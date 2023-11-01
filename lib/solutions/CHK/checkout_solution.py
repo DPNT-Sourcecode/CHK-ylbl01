f83382d34faaa335c5db2485eacbdc24dacde72c
sbx_user1051 <sbx_user1051@169.254.13.245>
2023-11-01 19:19:19 +0000

    Wed Nov 01 19:19:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,24 +10,24 @@ def checkout(skus):
         'C': {'price':20, 'offer':None},
         'D': {'price':15, 'offer':None},
         'E': {'price':60, 'offer':[{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'B'}]}
-
     }
-    basket_summary = Counter(skus)
-    basket_price = 0
-    for item, count in basket_summary.items():
-        if item in price_info:
-            details = price_info[item]
-            if details['offer'] and count >= details['offer']['quantity']:
-                multibuy_instances = count // details['offer']['quantity']
-                multibuy_leftover = count % details['offer']['quantity']
-                basket_price += multibuy_instances * details['offer']['discounted_price']
-                basket_price += multibuy_leftover * details['price']
-            else:
-                basket_price += count * details['price']
+    # basket_summary = Counter(skus)
+    # basket_price = 0
+    # for item, count in basket_summary.items():
+    #     if item in price_info:
+    #         details = price_info[item]
+    #         if details['offer'] and count >= details['offer']['quantity']:
+    #             multibuy_instances = count // details['offer']['quantity']
+    #             multibuy_leftover = count % details['offer']['quantity']
+    #             basket_price += multibuy_instances * details['offer']['discounted_price']
+    #             basket_price += multibuy_leftover * details['price']
+    #         else:
+    #             basket_price += count * details['price']
+    #
+    #     else:
+    #         return -1
+    # return basket_price
 
-        else:
-            return -1
-    return basket_price
 
 
 
