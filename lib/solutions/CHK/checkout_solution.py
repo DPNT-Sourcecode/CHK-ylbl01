73d88ccf609bb37b308a79f9ad76fc03ff761764
sbx_user1051 <sbx_user1051@169.254.13.245>
2023-11-01 21:11:55 +0000

    Wed Nov 01 21:11:55 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,7 +12,7 @@ def checkout(skus):
         'B': {'price':30, 'offer':[{'type':'multibuy','quantity':2,'discounted_price':45}]},
         'C': {'price':20, 'offer':None},
         'D': {'price':15, 'offer':None},
-        'E': {'price':40, 'offer':[{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'B'}]}
+        'E': {'price':40, 'offer':[{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'B'}]},
         'F': {'price':10, 'offer':[{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'F'}]}
     }
     basket_summary = Counter(skus)
@@ -42,9 +42,18 @@ def remove_free_items(item_counter,price_info):
             offer = price_info[item]['offer'][0] #item['offer'][0]
             if offer['type'] == 'buy_x_get_free':
                 free_item = offer['free_item']
-                number_of_free_items= (count // offer['buy']) * offer['get']
-                new_counter[free_item] -= number_of_free_items
-                new_counter[item] += count
+                if free_item == item:
+                    if count <= offer['buy']:
+                        new_counter[item] += count
+                    else:
+                        number_of_free_items= (count // offer['buy']) * offer['get']
+                        new_counter[free_item] -= number_of_free_items
+
+
+                else:
+                    number_of_free_items= (count // offer['buy']) * offer['get']
+                    new_counter[free_item] -= number_of_free_items
+                    new_counter[item] += count
             else:
                 new_counter[item] += count
         else:
@@ -76,6 +85,8 @@ def get_optimal_price_for_item(item, count,price, offer_list,price_info):
 
     return best_price
 
+basket = ['F']*3
+print(checkout(basket))
 
 
 
