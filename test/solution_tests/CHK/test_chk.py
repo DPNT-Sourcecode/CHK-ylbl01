d4aba484aeb9034e248973944068fbfaa8cc44ed
sbx_user1051 <sbx_user1051@169.254.13.245>
2023-11-01 18:58:17 +0000

    Wed Nov 01 18:58:17 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,12 +1,13 @@
-from solutions.CHK import checkout_solution
+from solutions.CHK.checkout_solution import checkout
 
 
 class TestCHK():
     def test_basket(self):
-        assert checkout_solution.checkout.compute(['A','A','A','A','B','B','C']) == 245
+        assert checkout(['A','A','A','A','B','B','C']) == 245
 
     def test_basket_fail(self):
-        assert checkout_solution.checkout.compute(['A','A','A','A','B','B','C','E']) == -1
+        assert checkout(['A','A','A','A','B','B','C','E']) == -1
+
 
 
 
