36bab9c346ef7122f98a7158382911870fe7dc06
sbx_user1051 <sbx_user1051@169.254.13.245>
2023-11-01 20:26:52 +0000

    Wed Nov 01 20:26:52 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,19 +21,6 @@ def checkout(skus):
             else:
                 basket_addition_from_item = get_optimal_price_for_item(item,count,details['price'],details['offer'],price_info)
             basket_price += basket_addition_from_item
-
-            # best_price = count * details['price']
-            # for offer in details.get('offer',[]):
-            #     if offer['type'] == 'multibuy' and count >= offer['quantity']:
-            #         multibuy_instances = count // offer['quantity']
-            #         multibuy_leftover = count % details['offer']['quantity']
-            # if details['offer'] and count >= details['offer']['quantity']:
-            #     multibuy_instances = count // details['offer']['quantity']
-            #     multibuy_leftover = count % details['offer']['quantity']
-            #     basket_price += multibuy_instances * details['offer']['discounted_price']
-            #     basket_price += multibuy_leftover * details['price']
-            # else:
-            #     basket_price += count * details['price']
         else:
             return -1
     return basket_price
@@ -67,17 +54,4 @@ def get_optimal_price_for_item(item, count,price, offer_list,price_info):
 
     return best_price
 
-# price_info = {
-#     'A': {'price':50, 'offer':[{'type':'multibuy', 'quantity':3,'discounted_price':130},{'type':'multibuy', 'quantity':5,'discounted_price':200}]},
-#     'B': {'price':30, 'offer':[{'type':'multibuy','quantity':2,'discounted_price':45}]},
-#     'C': {'price':20, 'offer':None},
-#     'D': {'price':15, 'offer':None},
-#     'E': {'price':60, 'offer':[{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'B'}]}
-# }
-# offer_list = [{'type':'buy_x_get_free','buy':2,'get':1,'free_item':'B'}]
-# #offer_list = [{'type': 'multibuy', 'quantity': 2, 'discounted_price': 45}]
-#print(get_optimal_price_for_item("C",9,20,offer_list,price_info))
-# 380 + 75 + 60
-basket =   ['C']*3 #+ ['A']*9 + ['B']*3 #+
-print(checkout(basket))
 
