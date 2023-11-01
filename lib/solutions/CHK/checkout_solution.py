45b3fe6c2e8f17c0aab27c2c0eae772cb6e78499
sbx_user1051 <sbx_user1051@169.254.13.245>
2023-11-01 18:35:55 +0000

    Wed Nov 01 18:35:55 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,11 @@
 # skus = unicode string
 def checkout(skus):
     price_info = {
-        "A": [50,[3,130]],
+        "A": {"price":150, 'offer':{'quantity':3,price}}
         "B": [30,[2,45]],
         "C": [20],
         "D": [15]
     }
-
 
 
 
